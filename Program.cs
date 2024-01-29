feae250c6332fc2d8d7c962acb7e43019764381a
csi22-irodhan <ivanrodriiguez04@gmail.com>
2024-01-29 14:22:17 +0100

    29/01/2024

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ejercicio1DateTime 
+{
+    /// <summary>
+    /// Clase principal de la aplicacion
+    /// irodhan -> 29/01/2024
+    /// </summary>
+    class Program 
+    {
+        /// <summary>
+        /// Metodo principal de la aplicacion
+        /// irodhan -> 29/01/2024
+        /// </summary>
+        static void Main(string[] args) 
+        {
+            //Creamos una variable y le añadimos la informacion
+            DateTime fechaActual = DateTime.Now;
+            //Le añadimos un formato en especifico
+            fechaActual.ToString("yy - MM - ddThh:mm: ss.ms");
+            //Mostramos el resultado
+            Console.WriteLine("Fecha formato-tiempo: "+fechaActual.ToString());
+
+        }
+    }
+}
